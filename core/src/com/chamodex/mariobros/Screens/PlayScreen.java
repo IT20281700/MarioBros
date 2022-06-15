fc6aa296c5ccffdc856c1915568e17a1d26a75db
IT20281700 <it20281700@my.sliit.lk>
2022-06-15 13:01:22 +0530

    Add super mario movements

--- core/src/com/chamodex/mariobros/Screens/PlayScreen.java
+++ core/src/com/chamodex/mariobros/Screens/PlayScreen.java
@@ -1,17 +1,29 @@
 package com.chamodex.mariobros.Screens;
 
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.maps.MapObject;
+import com.badlogic.gdx.maps.objects.RectangleMapObject;
 import com.badlogic.gdx.maps.tiled.TiledMap;
 import com.badlogic.gdx.maps.tiled.TmxMapLoader;
 import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
+import com.badlogic.gdx.math.Rectangle;
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.physics.box2d.Body;
+import com.badlogic.gdx.physics.box2d.BodyDef;
+import com.badlogic.gdx.physics.box2d.Box2DDebugRenderer;
+import com.badlogic.gdx.physics.box2d.FixtureDef;
+import com.badlogic.gdx.physics.box2d.PolygonShape;
+import com.badlogic.gdx.physics.box2d.World;
 import com.badlogic.gdx.utils.viewport.FitViewport;
 import com.badlogic.gdx.utils.viewport.Viewport;
 import com.chamodex.mariobros.MarioBros;
 import com.chamodex.mariobros.Scenes.Hud;
+import com.chamodex.mariobros.Sprites.Mario;
 
 public class PlayScreen implements Screen {
 
@@ -20,9 +32,20 @@ public class PlayScreen implements Screen {
     private Viewport gamePort;
     private Hud hud;
 
+    // Tiled map variables
     private TmxMapLoader mapLoader;
     private TiledMap map;
     private OrthogonalTiledMapRenderer renderer;
+
+    // Box2d variables
+    private World world;
+    private Box2DDebugRenderer b2dr;
+    private BodyDef bdef;
+    private PolygonShape shape;
+    private FixtureDef fdef;
+    private Body body;
+
+    private Mario mario;
 
     public PlayScreen(MarioBros game) {
         this.game = game;
@@ -30,15 +53,57 @@ public class PlayScreen implements Screen {
         gamecam = new OrthographicCamera();
 
         // Create a FitViewport to maintain virtual aspect ratio despite
-        gamePort = new FitViewport(MarioBros.V_WIDTH, MarioBros.V_HEIGHT, gamecam);
+        gamePort = new FitViewport(MarioBros.V_WIDTH / MarioBros.PPM, MarioBros.V_HEIGHT / MarioBros.PPM, gamecam);
 
         // Create our game HUD for scores/timers/level info
         hud = new Hud(game.batch);
 
+        // Load our map and setup our map renderer
         mapLoader = new TmxMapLoader();
         map = mapLoader.load("level1.tmx");
-        renderer = new OrthogonalTiledMapRenderer(map);
+        renderer = new OrthogonalTiledMapRenderer(map, 1 / MarioBros.PPM);
+
+        // Initially set our gamecam to be centered correctly at the start of map
         gamecam.position.set(gamePort.getWorldWidth()/2, gamePort.getWorldHeight()/2, 0);
+
+        world = new World(new Vector2(0, -10), true);
+        b2dr = new Box2DDebugRenderer();
+
+        bdef = new BodyDef();
+        shape = new PolygonShape();
+        fdef = new FixtureDef();
+
+        // Create ground bodies/fixtures
+        createObjects(2);
+
+        // Create pipe bodies/fixtures
+        createObjects(3);
+
+        // Create brick bodies/fixtures
+        createObjects(5);
+
+        // Create coin bodies/fixtures
+        createObjects(4);
+
+        // Create Mario
+        mario = new Mario(world);
+
+    }
+
+    // create objects
+    public void createObjects(int indexOfObject) {
+        for (MapObject object : map.getLayers().get(indexOfObject).getObjects().getByType(RectangleMapObject.class)) {
+            Rectangle rect = ((RectangleMapObject) object).getRectangle();
+
+            bdef.type = BodyDef.BodyType.StaticBody;
+            bdef.position.set((rect.getX() + rect.getWidth() / 2) / MarioBros.PPM, (rect.getY() + rect.getHeight() / 2) / MarioBros.PPM);
+
+            body = world.createBody(bdef);
+
+            shape.setAsBox(rect.getWidth() / 2 / MarioBros.PPM, rect.getHeight() / 2 / MarioBros.PPM);
+            fdef.shape = shape;
+            body.createFixture(fdef);
+        }
     }
 
     @Override
@@ -47,12 +112,22 @@ public class PlayScreen implements Screen {
     }
 
     private void handleInput(float dt) {
-        if(Gdx.input.isTouched())
-            gamecam.position.x += 100 * dt;
+
+        // If user is holding down mouse move our camera through the game world
+        if(Gdx.input.isKeyJustPressed(Input.Keys.UP) || Gdx.input.isKeyJustPressed(Input.Keys.W))
+            mario.b2Body.applyLinearImpulse(new Vector2(0, 4f), mario.b2Body.getWorldCenter(), true);
+        if(Gdx.input.isKeyPressed(Input.Keys.RIGHT) || Gdx.input.isKeyPressed(Input.Keys.D) && mario.b2Body.getLinearVelocity().x <= 2)
+            mario.b2Body.applyLinearImpulse(new Vector2(0.1f, 0), mario.b2Body.getWorldCenter(), true);
+        if(Gdx.input.isKeyPressed(Input.Keys.LEFT) || Gdx.input.isKeyPressed(Input.Keys.A) && mario.b2Body.getLinearVelocity().x >= -2)
+            mario.b2Body.applyLinearImpulse(new Vector2(-0.1f, 0), mario.b2Body.getWorldCenter(), true);
     }
 
     public void update(float dt) {
         handleInput(dt);
+
+        world.step(1/60f, 6,2);
+
+        gamecam.position.x = mario.b2Body.getPosition().x;
 
         gamecam.update();
         renderer.setView(gamecam);
@@ -66,7 +141,11 @@ public class PlayScreen implements Screen {
         Gdx.gl.glClearColor(0, 0, 0, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
+        // Render our game map
         renderer.render();
+
+        // Render our Box2DDebugLines
+        b2dr.render(world, gamecam.combined);
 
         // Set our batch to new draw what the Hud camera sees
         game.batch.setProjectionMatrix(hud.stage.getCamera().combined);
